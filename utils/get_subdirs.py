d4e8d968590746d9c568aaef9a2f841951cb7e4e
jon_chun <43786371+jon-chun@users.noreply.github.com>
2022-03-14 12:32:47 -0400

    new directory structure

--- utils/get_subdirs.py
+++ utils/get_subdirs.py
@@ -7,10 +7,13 @@ def get_subdirs(Corpus_Genre, Corpus_Type, NotebookModels):
 
   global FNAME_SENTIMENT_RAW
   global DIR_ROOT
+  global SUBDIR_SENTIMENTARCS
   global SUBDIR_TEXT_RAW
   global SUBDIR_TEXT_CLEAN
   global SUBDIR_SENTIMENT_RAW
   global SUBDIR_SENTIMENT_CLEAN
+  global SUBDIR_TIMESERIES_RAW
+  global SUBDIR_TIMESERIES_CLEAN
   global SUBDIR_PLOTS
   global SUBDIR_DATA
   global SUBDIR_UTILS
@@ -27,10 +30,13 @@ def get_subdirs(Corpus_Genre, Corpus_Type, NotebookModels):
     print(f'ERROR: Illegal value for NotebookModels: {NotebookModels}')
     return
 
-  SUBDIR_TEXT_RAW = f"./text_raw/{Corpus_Genre}_text_{Corpus_Type}_raw/"
-  SUBDIR_TEXT_CLEAN = f"./text_clean/{Corpus_Genre}_text_{Corpus_Type}_clean/"
-  SUBDIR_SENTIMENT_RAW = f"./sentiment_raw/{Corpus_Genre}_sentiment_{Corpus_Type}_raw/"
-  SUBDIR_SENTIMENT_CLEAN = f"./sentiment_clean/{Corpus_Genre}_sentiment_{Corpus_Type}_clean/"
+  SUBDIR_SENTIMENTARCS = '/gdrive/MyDrive/cdh/sentiment_arcs'
+  SUBDIR_TEXT_RAW = f"./text_raw/text_raw_{Corpus_Genre}_{Corpus_Type}"
+  SUBDIR_TEXT_CLEAN = f"./text_clean/text_clean_{Corpus_Genre}_{Corpus_Type}/"
+  SUBDIR_SENTIMENT_RAW = f"./sentiment_raw/sentiment_raw_{Corpus_Genre}_{Corpus_Type}/"
+  SUBDIR_SENTIMENT_CLEAN = f"./sentiment_clean/sentiemnt_clean_{Corpus_Genre}_{Corpus_Type}/"
+  SUBDIR_TIMESERIES_RAW = f"./timeseries_raw/timeseries_raw_{Corpus_Genre}_{Corpus_Type}/"
+  SUBDIR_TIMESERIES_CLEAN = f"./timeseries_clean/timeseries_clean_{Corpus_Genre}_{Corpus_Type}/"
   SUBDIR_PLOTS = f"./plots/"
   SUBDIR_DATA = f"./data/"
   SUBDIR_UTILS = f"./utils/"
@@ -40,17 +46,29 @@ def get_subdirs(Corpus_Genre, Corpus_Type, NotebookModels):
   print('Verify the Directory Structure:\n')
   print('-------------------------------\n')
 
-  print(f'          [Corpus Genre]: {Corpus_Genre}\n')
-  print(f'           [Corpus Type]: {Corpus_Type}\n\n')
+  print(f'           [Corpus Genre]: {Corpus_Genre}\n')
+  print(f'            [Corpus Type]: {Corpus_Type}\n\n')
 
-  print(f'   [FNAME_SENTIMENT_RAW]: {FNAME_SENTIMENT_RAW}\n\n')
-
-  print(f'       [SUBDIR_TEXT_RAW]: {SUBDIR_TEXT_RAW}\n')
-  print(f'     [SUBDIR_TEXT_CLEAN]: {SUBDIR_TEXT_CLEAN}\n')
-  print(f'  [SUBDIR_SENTIMENT_RAW]: {SUBDIR_SENTIMENT_RAW}\n')
-  print(f'[SUBDIR_SENTIMENT_CLEAN]: {SUBDIR_SENTIMENT_CLEAN}\n')
-  print(f'          [SUBDIR_PLOTS]: {SUBDIR_PLOTS}\n')
-  print(f'           [SUBDIR_DATA]: {SUBDIR_DATA}\n')
-  print(f'          [SUBDIR_UTILS]: {SUBDIR_UTILS}\n')
+  print(f'    [FNAME_SENTIMENT_RAW]: {FNAME_SENTIMENT_RAW}\n\n')
+  print('\n\nINPUTS:')
+  print('-------------------------------\n')
+  print(f'   [SUBDIR_SENTIMENTARCS]: {SUBDIR_SENTIMENTARCS}\n')
+  print('\nSTEP 1: Clean Text')
+  print('--------------------\n')
+  print(f'        [SUBDIR_TEXT_RAW]: {SUBDIR_TEXT_RAW}\n')
+  print(f'      [SUBDIR_TEXT_CLEAN]: {SUBDIR_TEXT_CLEAN}\n')
+  print('\nSTEP 2: Get Sentiments')
+  print('--------------------\n')
+  print(f'   [SUBDIR_SENTIMENT_RAW]: {SUBDIR_SENTIMENT_RAW}\n')
+  print(f' [SUBDIR_SENTIMENT_CLEAN]: {SUBDIR_SENTIMENT_CLEAN}\n')
+  print('\nSTEP 3: Smooth Time Series and Get Crux Points')
+  print('--------------------\n')
+  print(f'  [SUBDIR_TIMESERIES_RAW]: {SUBDIR_SENTIMENT_RAW}\n')
+  print(f'[SUBDIR_TIMESERIES_CLEAN]: {SUBDIR_SENTIMENT_CLEAN}\n')
+  print('\n\nOUTPUTS:')
+  print('-------------------------------\n')
+  print(f'           [SUBDIR_PLOTS]: {SUBDIR_PLOTS}\n')
+  print(f'            [SUBDIR_DATA]: {SUBDIR_DATA}\n')
+  print(f'           [SUBDIR_UTILS]: {SUBDIR_UTILS}\n')
 
   return
